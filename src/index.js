032074985c0e480dba603d9b8b0efc0437b6cb76
heaveek <heaveek@gmail.com>
2018-10-03 21:05:15 +0300

    79 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  function getMultiepler(base){
+    let n = base;
+    const nArr = [];
+      if(n == 2){
+        return n;
+      } else {
+          for(i = 2; i < n; i++) { 
+            while(n % i == 0) {
+              n /= i;  
+            }			      
+            if(n != 1) {
+              nArr.push(n);
+            }	else {
+              nArr.push(base);
+            }			
+          } 
+        }
+      nArr.sort((a,b) => a-b);
+      return nArr[0];
+    }
+  
+  let countZero = 0;  
+  for(let i = 1; number > Math.pow(getMultiepler(base), i); i++) {
+    countZero += Math.floor(number / Math.pow(getMultiepler(base), i));
+  }
+  return countZero;  
 }
